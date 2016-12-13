0b321b3d9527ba1b62729fec6bfe5c248da8f2c4
chollenbeck <chollenbeck07@gmail.com>
2016-12-13 09:08:10 -0500

    Updated version

--- rad_haplotyper.pl
+++ rad_haplotyper.pl
@@ -10,7 +10,7 @@ use List::Util qw/shuffle/;
 use Term::ProgressBar;
 use Parallel::ForkManager;
 
-my $version = '1.1.4';
+my $version = '1.1.5';
 
 my $command = 'rad_haplotyper ' . join(" ", @ARGV);
 
